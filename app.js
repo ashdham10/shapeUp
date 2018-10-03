10992d9dbe0d4a6746e66c867a5358ea40fa2581
ashdham10 <ashleydenham10@gmail.com>
2018-10-03 16:01:50 -0500

    day2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('dom content loaded!');
 
+    const MAX = 450;
+
     let shapeContainer = document.getElementById('shapeContainer');
     let circleBtn = document.getElementById('circleButton');
     let triangleBtn = document.getElementById('triangleButton');
@@ -8,64 +10,93 @@ document.addEventListener('DOMContentLoaded', function () {
     let squareBtn = document.getElementById('squareButton');
 
     class Shape {
-        constructor() {
+        constructor(x, y) {
             this.shape = document.createElement('div');
-            this.createShape();
-        }
-        createShape() {
             shapeContainer.appendChild(this.shape);
+            this.shape.style.left = `${x}px`;
+            this.shape.style.top = `${y}px`
+            this.removeShape();
+        }
+        removeShape(){
+            this.shape.addEventListener('dblclick', () => {
+                shapeContainer.removeChild(this.shape);
+            })
         }
     }
 
     class Circle extends Shape {
-        constructor(radius) {
-            super();
+        constructor(x, y) {
+            super(x, y);
             this.shape.classList.add('circle');
-            this.createCircle();
-        }
-        createCircle() {
-            circleBtn.addEventListener('click', () => {
-                let circle = new Circle(10);
-                console.log(circle);
-            }) 
+            this.shape.style.height = `${document.getElementById('circleInput').value}px`;
+            this.shape.style.width = `${document.getElementById('circleInput').value}px`;
+            console.log(this.shape.style.height);
         }
     }
 
     class Triangle extends Shape {
-        constructor(height) {
-            super();
+        constructor(x, y) {
+            super(x, y);
             this.shape.classList.add('triangle');
-
-        }
-        createTriangle() {
+            this.shape.style.borderBottomStyle = 'solid';
+            this.shape.style.borderBottomColor = 'blue';
+            this.shape.style.borderLeftStyle = 'solid';
+            this.shape.style.borderLeftColor = 'transparent';
+            this.shape.style.borderBottomWidth = `${document.getElementById('triangleInput').value}px`;
+            this.shape.style.borderLeftWidth = `${document.getElementById('triangleInput').value}px`;
 
         }
     }
 
     class Rectangle extends Shape {
-        constructor(height, width) {
-            super();
+        constructor(x, y) {
+            super(x, y);
             this.shape.classList.add('rectangle');
-
-        }
-        createRectangle() {
-
+            this.shape.style.height = `${document.getElementById('rectangleInputHeight').value}px`;
+            this.shape.style.width = `${document.getElementById('rectangleInputWidth').value}px`;
         }
     }
 
     class Square extends Shape {
-        constructor(sides) {
-            super();
+        constructor(x, y) {
+            super(x, y);
             this.shape.classList.add('square');
-        }
-        createSquare() {
-
+            this.shape.style.height = `${document.getElementById('squareInput').value}px`;
+            this.shape.style.width = `${document.getElementById('squareInput').value}px`;
         }
     }
 
+circleBtn.addEventListener('click', insertCircle);
+triangleBtn.addEventListener('click', insertTriangle);
+rectangleBtn.addEventListener('click', insertRectangle);
+squareBtn.addEventListener('click', insertSquare);
 
+function insertCircle(){
+    let xVal = randomLocation(0, MAX);
+    let yVal = randomLocation(0, MAX);
+    let cir = new Circle(xVal, yVal);
+}
 
+function insertTriangle(){
+    let xVal = randomLocation(0, MAX);
+    let yVal = randomLocation(0, MAX);
+    let tri = new Triangle(xVal, yVal);
+}
 
+function insertRectangle(){
+    let xVal = randomLocation(0, MAX);
+    let yVal = randomLocation(0, MAX);
+    let rec = new Rectangle(xVal, yVal);
+}
 
+function insertSquare(){
+    let xVal = randomLocation(0, MAX);
+    let yVal = randomLocation(0, MAX);
+    let sq = new Square(xVal, yVal);
+}
+
+function randomLocation(max, min){
+ return Math.floor(Math.random() * (max - min) + min);
+}
 
 });
